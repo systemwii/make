b6cc6cbdc7eb3c64ebd79a234cf18c69f323feb5
Filip Mašić <shoutplenty@gmail.com>
2024-11-29 08:55:03 +0000

    repartition files

--- rules/recipes.mk
+++ rules/recipes.mk
@@ -1,8 +1,23 @@
 include $(DEVKITPPC)/tools.mk
 
 #---------------------------------------------------------------------------------
-%.a:
+%.dol: %.elf
+	$(SILENTMSG) output ... $(notdir $@)
+	$(SILENTCMD)elf2dol $< $@
+
 #---------------------------------------------------------------------------------
+%.tpl: %.scf
+	$(SILENTMSG) $(notdir $<)
+	$(SILENTCMD)gxtexconv -s $< -d $(DEPSDIR)/$*.d -o $@
+
+#---------------------------------------------------------------------------------
+%.elf:
+	$(SILENTMSG) linking ... $(notdir $@)
+	$(ADD_COMPILE_COMMAND) end
+	$(SILENTCMD)$(LD)  $^ $(LDFLAGS) $(LIBPATHS) $(LIBS) -o $@
+
+#---------------------------------------------------------------------------------
+%.a:
 	$(SILENTMSG) $(notdir $@)
 	$(ADD_COMPILE_COMMAND) end
 	$(SILENTCMD)rm -f $@
@@ -37,3 +52,13 @@ include $(DEVKITPPC)/tools.mk
 	$(SILENTMSG) $(notdir $<)
 	$(ADD_COMPILE_COMMAND) add $(CC) "$(CPPFLAGS) $(ASFLAGS) -c $< -o $@" $<
 	$(SILENTCMD)$(CC) -MMD -MP -MF $(DEPSDIR)/$*.d -x assembler-with-cpp $(CPPFLAGS) $(ASFLAGS) -c $< -o $@ $(ERROR_FILTER)
+
+#---------------------------------------------------------------------------------
+# canned command sequence for binary data
+#---------------------------------------------------------------------------------
+define bin2o
+	$(eval _tmpasm := $(shell mktemp))
+	$(SILENTCMD)bin2s -a 32 -H `(echo $(<F) | tr . _)`.h $< > $(_tmpasm)
+	$(SILENTCMD)$(CC) -x assembler-with-cpp $(CPPFLAGS) $(ASFLAGS) -c $(_tmpasm) -o $(<F).o
+	@rm $(_tmpasm)
+endef
